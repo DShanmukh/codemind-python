e82807e28c9db3ba47abc165117e9f7d8a20ceb3
Shanmukh <96300319+DShanmukh@users.noreply.github.com>
2023-01-11 20:20:31 +0530

    n=int(input()) c=0 for i in range(0,10):     f=0     temp=n     while(temp!=0):         r=temp%10         if(i==r):             f+=1         temp=temp//10     if(f>1):         c+=1 if(c==0):     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,10 +1,9 @@
 n=int(input())
 c=0
-f=0
 for i in range(0,10):
-    temp=n
     f=0
-    while temp>0:
+    temp=n
+    while(temp!=0):
         r=temp%10
         if(i==r):
             f+=1
@@ -12,6 +11,6 @@ for i in range(0,10):
     if(f>1):
         c+=1
 if(c==0):
-    print("Unique Number");
+    print("Unique Number")
 else:
-    print("Not Unique Number");
+    print("Not Unique Number")
